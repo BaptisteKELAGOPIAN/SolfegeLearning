d91aaeccb658128d8c1831ec4ac57da425f379d1
Baptiste KELAGOPIAN <71840885+BaptisteKELAGOPIAN@users.noreply.github.com>
2023-02-10 16:49:05 +0100

    add mini game

--- solfege/src/routes/+page.svelte
+++ solfege/src/routes/+page.svelte
@@ -1,16 +1,47 @@
 <script>
     import { onMount } from 'svelte';
+	import { empty } from 'svelte/internal';
+    import { portee } from '../routes/portee';
 
     let list_note = ["do", "re", "mi", "fa", "sol", "la", "si"];
     let curr_note = "";
-    let time = 0;
-    let line = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    let portee = [line, line, line, line, line];
+    export let length_line = 500;
+    let pos = 0;
+    let wch_line = 0;
+    let score = 0; 
+
+    pos = length_line - 1;
+
+    function rand() {
+        return Math.floor(Math.random() * portee.length);
+    }
+
+    function reset() {
+        portee[wch_line].main = Array(length_line).fill(0);
+        pos = length_line - 1;
+        wch_line = rand();
+    }
+
+    /**
+	 * @param {string} [note]
+	 */
+    function click_note(note) {
+        if (portee[wch_line].name === note){
+            score += 1;
+            reset();
+        }
+    }
 
     onMount(() => {
 		const interval = setInterval(() => {
-			time += 1;
-		}, 1000);
+            rand();
+            portee[wch_line].main[pos] = 1;
+            portee[wch_line].main[pos + 1] = 0;
+            if (pos === 0) {
+                reset();
+            }
+            pos -= 1;
+		}, 10);
         return () => {
 			clearInterval(interval);
 		};
@@ -18,27 +49,37 @@
 </script>
 
 <section>
-    <p>{time}</p>
-    <p>{curr_note}</p>
-    <div class ="row_portee">
-        {#each portee as p}
-            <div class="line">
-                {#each p as l}
-                        <!-- if statement -->
-                        {#if l === 0}
-                            -
-                        {:else}
-                            o
-                        {/if}
-                {/each}
-            </div>
+    <!-- <p>portee[line 0 =] {portee[0]}</p>
+    <p>portee[line 1 =] {portee[1]}</p>
+    <p>portee[line 2 =] {portee[2]}</p> -->
+</section>
+
+<section>
+    <p>SCORE = {score}</p>
+    <div class ="main_portee">
+        {#each portee as line}
+        <div class = "portee">
+            {#each line.main as note}
+                <div class="line">
+                    {#if line.empty === true}
+                        <div class = "tiret">-</div>
+                    {/if}
+                    {#if line.empty === false}
+                        <div class = "tiret white">-</div>
+                    {/if}
+                    {#if note === 1}
+                        <div class="round"></div>
+                    {/if}
+                </div>
+            {/each}
+        </div>
         {/each}
     </div>
     <div class ="row_note">
         {#each list_note as note}
             <div class="note">
-                <!-- change "note"  if click on n-->
-                <button on:click={() => curr_note = note}>{note}</button>
+                <!-- call function click_note() -->
+                <button on:click={() => click_note(note)}>{note}</button>
             </div>
         {/each}
     </div>
@@ -46,19 +87,42 @@
 
 <!-- css section -->
 <style>
+    .main_portee {
+        display: flex;
+        flex-direction: column;
+        width : 100%;
+    }
+    .portee {
+        display: flex;
+        flex-direction: row;
+        width : 100%;
+    }
+    .white{
+        color: white;
+        z-index: -2;
+    }
+    .line{
+        display: flex;
+        align-items: center;
+        height: 8px;
+    }
+    .tiret {
+        font-size: 3px;
+        text-decoration-line: line-through;
+        text-decoration-thickness: 0.25rem;
+    }
     .row_note {
         display: flex;
         flex-direction: row;
+        width : 100%;
     }
-    .line {
-        display: flex;
-        flex-direction: row;
-        height: 50px;
-        font-size: 50px;
+    .round {
+        position:relative;
+        background-image: url("src/media/note.png");
+        background-size: contain;
+        height: 80px;
+        width: 17px;
+        z-index: -1;
     }
-    .row_portee {
-        display: flex;
-        flex-direction: column;
-        width : 800px;
-    }
+
 </style>
---
+++ solfege/src/routes/portee.js
@@ -0,0 +1,49 @@
+let length_line = 500;
+
+export let portee = [
+    {
+        name: "fa",
+        empty: true,
+        main : Array.from({length: length_line}, () => 0)
+    },
+    {
+        name: "mi",
+        empty: false,
+        main : Array.from({length: length_line}, () => 0)
+    },
+    {
+        name:"ré",
+        empty: true,
+        main : Array.from({length: length_line}, () => 0)
+    },
+    {
+        name: "do",
+        empty: false,
+        main : Array.from({length: length_line}, () => 0)
+    },
+    {
+        name: "si",
+        empty: true,
+        main : Array.from({length: length_line}, () => 0)
+    },
+    {
+        name: "la",
+        empty: false,
+        main : Array.from({length: length_line}, () => 0)
+    },
+    {
+        name: "sol",
+        empty: true,
+        main : Array.from({length: length_line}, () => 0)
+    },
+    {
+        name: "fa",
+        empty: false,
+        main : Array.from({length: length_line}, () => 0)
+    },
+    {
+        name: "mi",
+        empty: true,
+        main : Array.from({length: length_line}, () => 0)
+    },
+];
